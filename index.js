3f81d5799b8536ad8130e9ba927ab5439814926b
mbusenitz <mbusenitz@gmail.com>
2018-08-28 21:12:09 -0700

    who cares

---
+++ index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const cp = require("child_process"),
+  argv = require("yargs")
+    .usage("Usage: xc-sources --project [project] --scheme [scheme] --arch [arch]")
+    .demandOption(["project", "scheme"])
+    .default("arch", "arm64").argv;
+
+cp.exec(
+  `xcodebuild -dry-run -project ${argv.project} -scheme ${argv.scheme} ARCH=${
+    argv.arch
+  }`,
+  { stdio: "pipe" },
+  (error, stdout, stderr) => {
+    var match,
+      regex = /\s-c\s+(.*)?\s+-o\s/g;
+    while (match = regex.exec(stdout)) {
+      console.log(match[1]);
+    }
+  }
+);
